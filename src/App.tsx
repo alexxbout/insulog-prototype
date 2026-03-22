14f33440e5d238f36b5bee8453c60a78f30cd660
Alexandre <boutinaud.pro@gmail.com>
2026-03-22 17:54:06 +0100

    fixed github pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AppShell>
           <Routes>
             <Route path="/" element={<ChatDashboard />} />
